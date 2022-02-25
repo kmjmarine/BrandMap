e5610eec49b8b14364740631e37f149451cc4ff4
kmjmarine <kmjmarine@naver.com>
2022-02-25 18:46:26 +0900

    set basic

---
+++ BrandMap/ProductListViewContoroller.swift
@@ -0,0 +1,12 @@
+//
+//  ProductListViewContoroller.swift
+//  BrandMap
+//
+//  Created by kmjmarine on 2022/02/25.
+//
+
+import UIKit
+
+final class ProductListViewContoroller: UITableViewController {
+    
+}
